01ebc8a553a64245975e630f20f0c2135fdf4f0e
sumanth37z <sumanth7077@gmail.com>
2024-10-22 20:42:13 +0530

    Add files via upload
    Hangman is a classic word-guessing game where players try to guess a hidden word by suggesting letters one at a time. Each incorrect guess brings a step closer to completing a drawing of a hangman. The goal is to guess the word before the drawing is completed. It's a fun way to test vocabulary and spelling skills!

---
+++ hangmanlist.py
@@ -0,0 +1,36 @@
+#Words to use in game
+
+
+words = ("toyota",
+        "honda",
+        "ford",
+        "nissan",
+        "bmw",
+        "chevrolet",
+        "mercedes-benz",
+        "audi",
+        "volkswagen",
+        "hyundai",
+        "kia",
+        "tesla",
+        "subaru",
+        "jaguar",
+        "land rover",
+        "volvo",
+        "fiat",
+        "chrysler",
+        "dodge",
+        "buick",
+        "gmc",
+        "acura",
+        "infiniti",
+        "porsche",
+        "cadillac",
+        "lamborghini",
+        "ferrari",
+        "maserati",
+        "aston martin",
+        "mclaren")
+
+
+
